1bcc302bc8a733b14768cf06be55d84926cb22b0
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-11-26 19:35:41 +0000

    created an Apollo Provider to make every request work with the Apollo Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+     <Router>
       <>
         <Navbar />
         <Routes>
@@ -24,7 +48,8 @@ function App() {
           />
         </Routes>
       </>
-    </Router>
+     </Router>
+    </ApolloProvider>
   );
 }
 
